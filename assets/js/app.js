1e9182a3ed4a43c17dbf5534bfd46e1e198c3868
marasolarp <mariasolarp@gmail.com>
2018-02-08 12:16:21 -0300

    pagina de login mas function de validación

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,38 @@
+//funcion para validar login
+function login(){
+	var user = document.getElementById("user").value;
+	var pass = document.getElementById("password").value;
+
+	var btnSesion = document.getElementsById("sing")[0];
+
+
+	if(user == ""){
+		var errorU = document.createElement("span")
+		errorU.classList.add("error-u")
+		var textU = document.createTextNode("Ingrese Usuario")
+
+		var userbien = document.getElementsByClassName("form-group")[0];
+		errorU.appendChild(textU)
+		userbien.appendChild(errorU)
+	
+	}else{
+		window.location.href="index.html"
+	}
+
+	if((pass == "") || (pass == 123456) || (pass.length<5) || (pass.length>7)){
+		var errorP = document.createElement("span")
+		errorP.classList.add("error-p")
+		var textP = document.createTextNode("Ingrese Contraseña")
+
+		var passbien = document.getElementsByClassName("form-group")[0];
+		errorP.appendChild(textP)
+		passbien.appendChild(errorP)
+
+	}else{
+		window.location.href="index.html"
+	}
+}
+
 
 //  function para el drag and drop
 function drag(ev) {
@@ -13,3 +48,7 @@ function drop(ev) {
   var id_foto = ev.dataTransfer.getData('text');//ACA RECUPERAMOS LOS DATOS 
   ev.target.appendChild(document.getElementById(id_foto));
 }
+
+
+
+
